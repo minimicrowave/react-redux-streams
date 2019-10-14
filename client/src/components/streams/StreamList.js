61a75227ac37428032883ebd483535194efeaab4
Serene <unexpecte-d@hotmail.com>
2019-10-14 12:34:02 +0800

    Add functionality for StreamList

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,7 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchStreams } from '../../actions';
 
-const StreamList = () => {
-	return <div>StreamList</div>;
+class StreamList extends Component {
+	componentDidMount = () => {
+		this.props.fetchStreams();
+	};
+
+	renderCreate = () => {
+		if (this.props.isSignedIn) {
+			return (
+				<div style={{ textAlign: 'right' }}>
+					<Link className="ui button primary" to="/streams/new">
+						Create Stream
+					</Link>
+				</div>
+			);
+		}
+	};
+
+	renderAdminButtons = (userId) => {
+		if (userId === this.props.currentUserId) {
+			return (
+				<div className="right floated content">
+					<button className="ui button primary">Edit</button>
+					<button className="ui button negative">Delete</button>
+				</div>
+			);
+		}
+	};
+
+	renderList = () => {
+		return this.props.streams.map(({ id, title, description, userId }) => (
+			<div className="item" key={id}>
+				{this.renderAdminButtons(userId)}
+				<i className="large middle aligned icon camera" />
+				<div className="content">
+					{title}
+					<div className="description">{description}</div>
+				</div>
+			</div>
+		));
+	};
+
+	render() {
+		return (
+			<div>
+				StreamList
+				<div className="ui celled list">{this.renderList()}</div>
+				{this.renderCreate()}
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = ({ stream, auth }) => {
+	return {
+		streams: Object.values(stream),
+		currentUserId: auth.userId,
+		isSignedIn: auth.isSignedIn
+	};
 };
 
-export default StreamList;
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
