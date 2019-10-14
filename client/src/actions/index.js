e129ef623f3c7392e4238141253002303db6a36d
Serene <unexpecte-d@hotmail.com>
2019-10-14 11:42:25 +0800

    Scaffold redux actions and reducers

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,13 @@
-import { SIGN_IN, SIGN_OUT } from './types';
+import streams from '../apis/streams';
+import {
+	SIGN_IN,
+	SIGN_OUT,
+	CREATE_STREAM,
+	FETCH_STREAM,
+	FETCH_STREAMS,
+	DELETE_STREAM,
+	EDIT_STREAM
+} from './types';
 
 const signIn = (userId) => {
 	return {
@@ -13,4 +22,29 @@ const signOut = () => {
 	};
 };
 
-export { signIn, signOut };
+const createStream = (formValues) => async (dispatch) => {
+	const { data } = await streams.post('/streams', formValues);
+	dispatch({ type: CREATE_STREAM, payload: data });
+};
+
+const fetchStreams = () => async (dispatch) => {
+	const { data } = await streams.get('/streams');
+	dispatch({ type: FETCH_STREAMS, payload: data });
+};
+
+const fetchStream = (id) => async (dispatch) => {
+	const { data } = await streams.get(`/stream/${id}`);
+	dispatch({ type: FETCH_STREAM, payload: data });
+};
+
+const editStream = (id, formValues) => async (dispatch) => {
+	const { data } = await streams.put(`/stream/${id}`, formValues);
+	dispatch({ type: EDIT_STREAM, payload: data });
+};
+
+const deleteStream = (id) => async (dispatch) => {
+	await streams.delete(`/stream/${id}`);
+	dispatch({ type: DELETE_STREAM, payload: id });
+};
+
+export { signIn, signOut, createStream, fetchStream, fetchStreams, editStream, deleteStream };
---
+++ client/src/reducers/streamReducer.js
@@ -0,0 +1,25 @@
+import {
+	CREATE_STREAM,
+	FETCH_STREAM,
+	FETCH_STREAMS,
+	EDIT_STREAM,
+	DELETE_STREAM
+} from '../actions/types';
+import _ from 'lodash';
+
+export default (state = {}, { type, payload }) => {
+	switch (type) {
+		case CREATE_STREAM:
+			return { ...state, [payload.id]: payload };
+		case FETCH_STREAM:
+			return { ...state, [payload.id]: payload };
+		case EDIT_STREAM:
+			return { ...state, [payload.id]: payload };
+		case FETCH_STREAMS:
+			return { ...state, ..._.mapkeys(payload, 'id') };
+		case DELETE_STREAM:
+			return _.omit(state, payload);
+		default:
+			return state;
+	}
+};
